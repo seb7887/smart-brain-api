1067a2399c228fa450da4f2c748a402009253725
Seba <seb7887@gmail.com>
2018-08-15 02:03:32 -0300

    Updated port and apis

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '59027a69fb494678a1952d8cc71022e4'
 });
 
 const handleApiCall = (req, res) => {
