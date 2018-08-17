1a4fe2a49453e05b3a1eb00b9529f0d553536fea
Seba <ssegura@inti.gob.ar>
2018-08-17 16:07:09 -0300

    Profile update implemented

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -3,4 +3,4 @@
 \i '/docker-entrypoint-initdb.d/tables/login.sql'
 
 -- For testing purposes only. This file will add dummy data
---\i '/docker-entrypoint-initdb.d/seed/seed.sql'
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
